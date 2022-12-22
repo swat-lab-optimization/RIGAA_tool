7c478d2359f7e7d066821fba24f7b512e8180b21
dgumenyuk <gumenyuk98@gmail.com>
2022-12-21 19:45:25 -0500

    Changing processing

--- rigaa/utils/get_stats.py
+++ rigaa/utils/get_stats.py
@@ -20,7 +20,7 @@ def get_stats(res, problem, algo):
     results = []
     population = -res.history[gen].pop.get("F")
     if algo != "nsga2" and algo != "rigaa":
-        population = sorted(population, key=lambda x: abs(x[0].fitness), reverse=True)
+        population = sorted(population, key=lambda x: x[0], reverse=True)
     for i in range(cf.ga["test_suite_size"]):
 
 
