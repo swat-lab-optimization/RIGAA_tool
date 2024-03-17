bc510557d23332b7553cebe5e34bed32fbab1b97
dgumenyuk <log6305.poly@gmail.com>
2024-03-16 23:32:30 -0400

    Adding hypervolume calculation

--- rigaa/utils/get_convergence.py
+++ rigaa/utils/get_convergence.py
@@ -6,7 +6,7 @@ Description: script for getting the convergence of the search algoritm (best val
 
 import numpy as np
 import config as cf
-
+from pymoo.indicators.hv import HV
 
 def get_convergence(res, n_offsprings):
     """
@@ -16,12 +16,28 @@ def get_convergence(res, n_offsprings):
       res: the result of the genetic algorithm
     """
     res_dict = {}
+    hyper_dict = {}
     generations = np.arange(0, len(res.history), 1)
     convergence = []
+    hyper_volume = []
+    if res.problem._name == "vehicle":
+        ref_point = np.array([16, 1])
+    elif res.problem._name == "robot":
+        ref_point = np.array([280, 1])
+    else:
+        ref_point = np.array([1, 1])
+   
     for gen in generations:
         population = -res.history[gen].pop.get("F")
         population = sorted(population, key=lambda x: x[0], reverse=True)
         convergence.append(population[0][0])
+
+        opt_num = len(res.history[gen].opt)
+        pareto = res.history[gen].pop.get("F")[:opt_num]*(-1)
+        
+        hv = HV(ref_point=ref_point) 
+        print(hv(pareto))
+        hyper_volume.append(hv(pareto))
 
     step = n_offsprings
     evaluations = np.arange(
@@ -30,4 +46,6 @@ def get_convergence(res, n_offsprings):
 
     for i in range(len(evaluations)):
         res_dict[str(evaluations[i])] = convergence[i]
-    return res_dict
+    for i in range(len(evaluations)):
+        hyper_dict[str(evaluations[i])] = hyper_volume[i]
+    return res_dict, hyper_dict
